328e62b22f8079e2c7dfc057e8c9bc2eb5b56e0d
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-24 14:37:12 +0530

    new commit

---
+++ study/img_srol.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct PRO: View {
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(0..<10) { _ in
+                        VStack {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    ForEach(0..<10) { _ in
+                                        VStack {
+                                            Image("fedhar")
+                                                .resizable()
+                                                .frame(width: 100, height: 100)
+                                                .clipShape(Circle())
+                                        }
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                        VStack {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    ForEach(0..<10) { _ in
+                                        VStack {
+                                            Image("blode bard")
+                                                .resizable()
+                                                .frame(width: 100, height: 100)
+                                                .clipShape(Circle())
+                                        }
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)         }
+    }
+}
+
+#Preview {
+    PRO()
+}
