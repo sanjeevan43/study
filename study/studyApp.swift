536a6bee44461310a0df3d126bd281b3a49d9733
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-24 14:32:28 +0530

    Initial Commit

---
+++ study/studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyApp.swift
+//  study
+//
+//  Created by sanjeevan on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
